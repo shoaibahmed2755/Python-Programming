92303698c99675b9c03c3628c611ce9a93c64bc2
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-11-26 07:25:08 +0530

    lenovo

--- python/v3.py
+++ python/v3.py
@@ -10,3 +10,16 @@
 #number --> int (-n to +n)
 #number --> float (-n.n to +n.n)
 #number --> complex (real+imaginary part)
+
+# a,b=-2343,3432
+# print(a,type(a))
+# print(b,type(b))
+
+# a,b=-2.343,34.32
+# print(a,type(a))
+# print(b,type(b))
+
+a=5+9j
+a=complex(3,5)
+print(a,type(a))
+print(a.real,a.imag)
