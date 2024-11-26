5782c5c9d9285744d210e30d096faa41182c54a7
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-11-26 07:14:41 +0530

    lenovo

--- python/v3.py
+++ python/v3.py
@@ -1,7 +1,7 @@
 # a=0
 # print("a is of Type/class :",type(a))
 
-a,b,c=2,3,2
-print(a)
-print(b)
-print(c)
+# a,b,c=2,3,2
+# print("a:",a,"id:",id(a))
+# print("a:",b,"id:",id(b))
+# print("a:",c,"id:",id(c))
