fe38781ac5bf28312734e07b203d2a7148b17e80
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-11-26 07:18:53 +0530

    lenovo

--- python/v3.py
+++ python/v3.py
@@ -5,3 +5,8 @@
 # print("a:",a,"id:",id(a))
 # print("a:",b,"id:",id(b))
 # print("a:",c,"id:",id(c))
+
+#datatypes in python :
+#number --> int (-n to +n)
+#number --> float (-n.n to +n.n)
+#number --> complex (real+imaginary part)
