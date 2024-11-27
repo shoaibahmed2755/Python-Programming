ae8a593e370b2cbf508a4aef5766fc820afffd08
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-11-27 23:36:36 +0530

    lenovo

--- python/v3.py
+++ python/v3.py
@@ -19,7 +19,21 @@
 # print(a,type(a))
 # print(b,type(b))
 
-a=5+9j
-a=complex(3,5)
-print(a,type(a))
-print(a.real,a.imag)
+# a=5+9j
+# a=complex(3,5)
+# print(a,type(a))
+# print(a.real,a.imag)
+
+# x = 1
+# y = 3565622.2554887711
+# z = -3255522
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
