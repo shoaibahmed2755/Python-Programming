4baa7eb0ab9272fcd1f60c13183f29fb01d55ff8
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-12-03 23:43:47 +0530

    lenovo

---
+++ python/listPY.py
@@ -0,0 +1,7 @@
+# # list is a collection of heterohrneous types of data :-
+# # can store same of diffrent types of data in a list 
+# # ex:- 1,5.5,'abc',67,28.0
+# # list1=[1,5.5,'abc',67,28.0]
+
+# # sice operator:-
+# syntax a[1,2,3,4,5,6] :- a[start:stop:end]
