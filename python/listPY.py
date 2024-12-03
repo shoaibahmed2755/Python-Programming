75548c3451ce0450672e3c81a44f01413ca95f52
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-12-03 23:50:56 +0530

    lenovo

--- python/listPY.py
+++ python/listPY.py
@@ -1,10 +1,11 @@
-# # list is a collection of heterohrneous types of data :-
-# # can store same of diffrent types of data in a list 
-# # ex:- 1,5.5,'abc',67,28.0
-# # list1=[1,5.5,'abc',67,28.0]
+# list is a collection of heterohrneous types of data :-
+# can store same of diffrent types of data in a list 
+# ex:- 1,5.5,'abc',67,28.0
+# list1=[1,5.5,'abc',67,28.0]
 
-# # sice operator:-
+# sice operator:-
 # syntax a[1,2,3,4,5,6] :- a[start:stop:end]
 
-a=[1,2,3,4,5,6]
-print(a)
+# a=[1,2,3,4,5,6]
+# print(a)
+# print(a,type(a))
