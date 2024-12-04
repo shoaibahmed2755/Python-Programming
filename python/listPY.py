d9865af84d3e74498a25909171ff6a4c0753afa2
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-12-04 18:51:24 +0530

    lenovo

--- python/listPY.py
+++ python/listPY.py
@@ -4,8 +4,12 @@
 # list1=[1,5.5,'abc',67,28.0]
 
 # sice operator:-
-# syntax a[1,2,3,4,5,6] :- a[start:stop:end]
+# syntax a[1,2,3,4,5,6] :- a[start:stop:step]
 
 # a=[1,2,3,4,5,6]
 # print(a)
 # print(a,type(a))
+
+a=[2,64,25,81,'abc',True]
+print(a[1:4:1])
+print(a[-5:-2:1])
