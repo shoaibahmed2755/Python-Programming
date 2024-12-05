b0d172beba8fb065f275e60b99906efc0d160bba
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-12-05 13:58:05 +0530

    lenovo

---
+++ python/listMethods.py
@@ -0,0 +1,4 @@
+#Adding elements inside the list 
+#  1.append() --> add 1 ele at the last of the list 
+#  2.insert() --> add 1 ele at the specified position
+#  3.extend() --> add multiple elements at the last of the list
