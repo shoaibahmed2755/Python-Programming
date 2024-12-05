d8b8b95f95afd56fdec8be6414cb0b1c853ecbb5
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-12-05 14:11:45 +0530

    lenovo

--- python/listMethods.py
+++ python/listMethods.py
@@ -4,9 +4,23 @@
 #  3.extend() --> add multiple elements at the last of the list
 a=[3,11,56,67,45,90,"abc"]
 print(a)
-a.append(100)
-print(a)
-a.append(True)
-print(a)
-a.append([1,2])
+
+# append() -->
+# a.append(100)
+# print(a)
+# a.append(True)
+# print(a)
+# a.append([1,2])
+# print(a)
+# print(a[::-1]) #reversing the list
+
+# insert() -->
+# a.insert(1,100)
+# print(a)
+# a.insert(3,[1,2,3])
+# print(a)
+# a.insert(-1,200)
+# print(a)
+
+a.extend([11,22,33])
 print(a)
