996080a6441bacac47328e64ad984f08bb47b9d4
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2024-12-05 14:01:44 +0530

    lenovo

--- python/listMethods.py
+++ python/listMethods.py
@@ -2,3 +2,11 @@
 #  1.append() --> add 1 ele at the last of the list 
 #  2.insert() --> add 1 ele at the specified position
 #  3.extend() --> add multiple elements at the last of the list
+a=[3,11,56,67,45,90,"abc"]
+print(a)
+a.append(100)
+print(a)
+a.append(True)
+print(a)
+a.append([1,2])
+print(a)
