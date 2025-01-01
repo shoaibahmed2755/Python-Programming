bee87f3082ed5a1ff220a1eb9856853b4b6a622c
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2025-01-01 19:17:53 +0530

    lenovo

--- python/other_methods.py
+++ python/other_methods.py
@@ -5,11 +5,24 @@
 # SORT() -> SORT THE LIST IN ASCENDING ORDER OR DESCENDING ORDER 
 # COPY() -> CREATE A COPY OF THE LIST
 
-a=[2,7,4,9,2,5,6,5,2,2,7,1]
+# a=[2,7,4,9,2,5,6,5,2,2,7,1]
+# print(a)
+# print(a.count(2)) # COUNT THE NUMBER OF OCCURENCE OF 2 IN THE LIST
+# print(a.index(7)) # ACCESS THE INDEX OF 2 IN THE LIST
+# a.reverse()
+# print(a)
+# a.sort(reverse=False)
+# print(a)
+
+a=[11,22]
+b=[33,44]
+c=a.copy()
+d=a
+print(a,id(a))
+print(b,id(b))
+print(c,id(c))
+print(d,id(d)) # id() function returns the “identity” of the object, which is
+a.append(45)
 print(a)
-print(a.count(2)) # COUNT THE NUMBER OF OCCURENCE OF 2 IN THE LIST
-print(a.index(7)) # ACCESS THE INDEX OF 2 IN THE LIST
-a.reverse()
-print(a)
-a.sort(reverse=False)
-print(a)
+print(b)
+print(c)
