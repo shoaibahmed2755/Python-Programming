68eb26ace72d5e747a4c95d9921eefa71aa493e8
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2025-01-01 18:56:49 +0530

    lenovo

--- python/other_methods.py
+++ python/other_methods.py
@@ -5,3 +5,11 @@
 # SORT() -> SORT THE LIST IN ASCENDING ORDER OR DESCENDING ORDER 
 # COPY() -> CREATE A COPY OF THE LIST
 
+a=[2,7,4,9,2,5,6,5,2,2,7,1]
+print(a)
+print(a.count(2)) # COUNT THE NUMBER OF OCCURENCE OF 2 IN THE LIST
+print(a.index(7)) # ACCESS THE INDEX OF 2 IN THE LIST
+a.reverse()
+print(a)
+a.sort(reverse=False)
+print(a)
