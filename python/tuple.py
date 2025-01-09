82cbb4f1c737d67fc11a57f0e94ddf4fd9bc5cdc
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2025-01-09 20:20:59 +0530

    lenovo

--- python/tuple.py
+++ python/tuple.py
@@ -5,3 +5,22 @@
 # tuple is faster than list because it is immutable.
 # tuple is more memory efficient than list because it is immutable.
 
+# Indexing a Tuple:
+
+# Tuple: (10, 20, 30, 40, 50)
+
+# Index:  0    1    2    3    4
+#         +----+----+----+----+----+
+#         | 10 | 20 | 30 | 40 | 50 |
+#         +----+----+----+----+----+
+
+# To access a value, use the index:
+# my_tuple[2] = 30
+# my_tuple[4] = 50
+
+# Example in Python:
+
+# my_tuple = (10, 20, 30, 40, 50)
+# print(my_tuple[2])  :: Output will be 30
+
+# Press any key to continue . . .
