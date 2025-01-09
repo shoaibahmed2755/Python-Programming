0c67408181e6d475fd89767dfc8c6dd5db39c7b9
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2025-01-09 20:20:22 +0530

    lenovo

---
+++ python/tuple.py
@@ -0,0 +1,7 @@
+# tuple : collection of heterogenious type of data which is indexed
+# ordered, it is immutable (unchangeable), 
+# creating a tuple we need to use parentheses () and we can't change the value of tuple after it is created.
+# example: a=(2,4,5.3,...ETC)
+# tuple is faster than list because it is immutable.
+# tuple is more memory efficient than list because it is immutable.
+
