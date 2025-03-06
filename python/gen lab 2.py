a2f822f66465279d0bc79cdfcc20cc49d79da326
shoaibahmed2755 <168957132+shoaibahmed2755@users.noreply.github.com>
2025-03-06 18:59:10 +0530

    lenovo

---
+++ python/gen lab 2.py
@@ -0,0 +1,6 @@
+import numpy as np
+import gensim
+from gensim.models.keyedvectors import KeyedVectors
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
